fd8070ac58c5f47dae0a6d35ea6c4c27c7e82888
shreewongtmg123-cloud <shreewongtmg123@gmail.com>
2025-12-11 21:24:10 +0545

    added exercise

--- homepage.php
+++ homepage.php
@@ -55,7 +55,7 @@
                     <li>All Premium Features</li>
                     <li>Yoga Classes</li>
                     <li>Massage Therapy</li>
-                    <li>Exercise</li>
+                    <li>exercise</li>
                 </ul>
             </div>
         </div>
