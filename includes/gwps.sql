67545f50d492c487cabb0f79f6c8b3f35809ad92
Ridesh_Maharjan <arkrideshmaharjan@gmail.com>
2025-12-28 18:30:19 +0545

    forgot sql

--- includes/gwps.sql
+++ includes/gwps.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 27, 2025 at 03:55 PM
+-- Generation Time: Dec 28, 2025 at 01:44 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -44,7 +44,8 @@ CREATE TABLE `packages` (
 
 INSERT INTO `packages` (`id`, `name`, `description`, `price`, `duration`, `category`, `created_by`, `created_at`) VALUES
 (1, 'Weight Loss Program', '30-day weight loss challenge', 299.00, '30 days', 'weight_loss', 1, '2025-12-27 13:07:12'),
-(2, 'Muscle Building', '12-week strength training', 199.00, '12 weeks', 'strength', 1, '2025-12-27 13:07:12');
+(2, 'Muscle Building', '12-week strength training', 199.00, '12 weeks', 'strength', 1, '2025-12-27 13:07:12'),
+(5, 'bisep program', '6x pushup', 199.00, '7 days', 'muscle_building', 6, '2025-12-27 16:06:34');
 
 -- --------------------------------------------------------
 
@@ -58,16 +59,18 @@ CREATE TABLE `purchases` (
   `package_id` int(11) NOT NULL,
   `purchase_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `amount` decimal(10,2) NOT NULL,
-  `status` enum('pending','completed') DEFAULT 'completed'
+  `status` enum('pending','completed') DEFAULT 'completed',
+  `is_active` tinyint(1) DEFAULT 1,
+  `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `purchases`
 --
 
-INSERT INTO `purchases` (`id`, `user_id`, `package_id`, `purchase_date`, `amount`, `status`) VALUES
-(2, 1, 1, '2025-12-27 13:40:48', 2999.00, 'completed'),
-(3, 1, 2, '2025-12-27 13:41:29', 4999.00, 'completed');
+INSERT INTO `purchases` (`id`, `user_id`, `package_id`, `purchase_date`, `amount`, `status`, `is_active`, `deleted_at`) VALUES
+(7, 8, 5, '2025-12-28 12:07:34', 199.00, 'completed', 1, NULL),
+(8, 1, 1, '2025-12-28 12:22:33', 299.00, 'completed', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -91,7 +94,9 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `full_name`, `email`, `phone`, `password`, `created_at`, `role`) VALUES
 (1, 'ridesh', 'Arkrideshmaharjan@gmail.com', '9000000000', '$2y$10$UUJG1T3z4hF.1TpGZzdgyeax9tXx7Pecf239khSmcjS4uGcLufIKG', '2025-12-13 09:23:21', 'user'),
-(6, 'ARK', 'adminridesh@gmail.com', '9821139366', '$2y$10$hdsLNbVzqChfMaPT4/Q2TOcJ3Gi3eJROUNEGNqxOZ8Yzldi9FekGi', '2025-12-18 08:25:35', 'admin');
+(6, 'ARK', 'adminridesh@gmail.com', '9821139366', '$2y$10$hdsLNbVzqChfMaPT4/Q2TOcJ3Gi3eJROUNEGNqxOZ8Yzldi9FekGi', '2025-12-18 08:25:35', 'admin'),
+(7, 'radish', 'kera@gmail.com', '9876543210', '$2y$10$TwzW23wXPULVcGDNktZnKeGkjZ0JkqYO0e33gnEnWH8pvZ4YnqNjO', '2025-12-27 15:17:49', 'user'),
+(8, 'sakar shahi', 'shakarshahi@gmail.com', '9870654321', '$2y$10$sRoIckGQwXcOqytillxQDuPAd17BwWZTeCTpWaqvZhOId3H6sNHbW', '2025-12-28 12:04:55', 'user');
 
 --
 -- Indexes for dumped tables
@@ -127,19 +132,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `packages`
 --
 ALTER TABLE `packages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `purchases`
 --
 ALTER TABLE `purchases`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Constraints for dumped tables
